62f053ce9e0ed6108f75289e562dc685a5f1219e
StigAkl <stg@hotmail.no>
2019-06-03 01:18:55 +0200

    Build files

---
+++ build/precache-manifest.2a98fdf2d5a88a47edbebf4e67e9c592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fc637415e34dd7487db8903552cf4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7e5cca6b1283b82a77",
+    "url": "/static/css/main.0459055b.chunk.css"
+  },
+  {
+    "revision": "e708796388deee6d73fd",
+    "url": "/static/js/2.cf45c052.chunk.js"
+  },
+  {
+    "revision": "7a7e5cca6b1283b82a77",
+    "url": "/static/js/main.ab394723.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
