130559262caa79fe7c389c7fb9b41eb1f6bef793
StigAkl <stg@hotmail.no>
2019-06-03 03:18:52 +0200

    Fix scheduler

--- server/scheduled_jobs/itemshop-schedule.js
+++ server/scheduled_jobs/itemshop-schedule.js
@@ -4,24 +4,49 @@ require('./../db/mongoose');
 const Item = require("../entities/item"); 
 
 
+
+function updateDatabase(name, rarity, price, image, date) {
+    options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    Item.findOneAndUpdate({name: name}, {
+        name: name,
+        rarity: rarity,
+        price: price,
+        image: image,
+        lastSeen: date
+    }, options, 
+    
+    function(error, result) {
+        if (error) {
+            console.log("Something went wrong:", error); 
+            return; 
+        }
+        console.log("Success: " + name + " updated"); 
+    });
+}
+
 axios.get(api_url, {
     headers: {
-        "x-api-key": process.env.FNBR_API_KEY
+        "x-api-key": process.env.FNBR_API_KEY || "024011f1-bc2b-41da-9c08-84c7350f10a1"
     }
 }).then((result) => {
     const featured = result.data.data.featured;
     const daily = result.data.data.daily; 
     const date = result.data.data.date; 
 
-    options = { upsert: true, new: true, setDefaultsOnInsert: true };
-
     if(featured.length > 0) {
         featured.forEach((item) => {
             if(item.type.toLowerCase() === "outfit") {
-                console.log(item.name); 
-                console.log(item.rarity.charAt(0).toUpperCase() + item.rarity.slice(1)); 
-                console.log(item.price.replace(",",""));
-                console.log(item.images.icon);
+                const name = item.name; 
+
+                const rarity = item.rarity.charAt(0).toUpperCase() + item.rarity.slice(1); 
+                const price = item.price.replace(",","");
+                let image = item.images.png; 
+
+                if(item.images.png === "false" || item.images.png === false) {
+                    image = item.images.icon; 
+                }
+
+                updateDatabase(name, rarity, price, image, date);
             }
         });
     }
@@ -38,25 +63,12 @@ axios.get(api_url, {
                 if(item.images.png === "false" || item.images.png === false) {
                     image = item.images.icon; 
                 }
-                
-                
-                Item.findOneAndUpdate({name: name}, {
-                    name: name,
-                    rarity: rarity,
-                    price: price,
-                    image: image,
-                    lastSeen: date
-                }, options, 
-                
-                function(error, result) {
-                    if (error) {
-                        console.log("Something went wrong:", error); 
-                        return; 
-                    }
-                    console.log("Success: " + name + " updated"); 
-                });
+
+                updateDatabase(name, rarity, price, image, date); 
             }
         });
+
+        
     }
 
 })
