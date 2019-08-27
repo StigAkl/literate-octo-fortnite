b3aa61d4f7c3d718d720935ca277561bc6002739
StigAkl <stg@hotmail.no>
2019-08-27 17:05:36 +0200

    afsd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-const connectionUrl = process.env.MONGODB_URI || "mongodb://heroku_g461gk83:278ejiercpuo6lihadr1fbbqss@ds263876.mlab.com:63876/heroku_g461gk83"
+const connectionUrl = process.env.MONGODB_URI || "asd"
 
 mongoose.set('useFindAndModify', false); 
 mongoose.connect(connectionUrl, {
